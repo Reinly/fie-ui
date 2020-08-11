f8a8295d54262e39ab23cd9b3b6ad17a06c4b807
Rein <1097101704@qq.com>
2020-08-11 16:52:26 +0800

    Button is finish

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked:value}">
+  <button class="gulu-switch" @click="toggle" :class="{'gulu-checked':value}">
     <span></span>
   </button>
 </template>
@@ -14,27 +14,49 @@ export default {
       context.emit("update:value", !props.value);
     };
     return { toggle };
-  }
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-button {
-  height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
+.gulu-switch {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #bfbfbf;
+  border-radius: $h/2;
+  position: relative;
   > span {
-    position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
   }
-  &.checked { background: #1890ff;
-    > span { left: calc(100% - #{$h2} - 2px); }
+  &.gulu-checked {
+    background: #1890ff;
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
   }
-  &:focus { outline: none; }
+  &:focus {
+    outline: none;
+  }
   &:active {
-    > span { width: $h2 + 4px; }
+    > span {
+      width: $h2 + 4px;
+    }
   }
-  &.checked:active {
-    > span { width: $h2 + 4px; margin-left: -4px; }
+  &.gulu-checked:active {
+    > span {
+      width: $h2 + 4px;
+      margin-left: -4px;
+    }
   }
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.scss'
-import { router } from './router';
+import "./lib/gulu.scss";
+import "./index.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./router";
 
-const app =  createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
