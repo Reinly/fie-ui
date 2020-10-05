172c30a4930f0ae968ff233e5335b70066986464
Rein <1097101704@qq.com>
2020-10-05 14:50:54 +0800

    modiffdied and add some Dialog

---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="dialog-overlay"></div>
+<div class="dialog-wrapper">
+    <header>标题</header>
+    <main>
+        <p>第一行数字</p>
+        <p>第二行数字</p>
+    </main>
+    <footer>
+        <Button>OK</Button>
+        <Button>Cancel</Button>
+    </footer>
+</div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+    components: {
+        Button,
+    },
+};
+</script>
